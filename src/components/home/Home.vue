a6faa00308bc57c4436737ae022d7ef1148c3362
JC Romualdo <juan.romualdo@zoro.com>
2019-05-07 11:53:38 -0500

    Home changes and indicator icons

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -7,28 +7,35 @@
           >
           <div 
           class="incident_item"
-          v-for="i in openIncidents"
-          :key="i.id"
+          v-for="incident in openIncidents"
+          :key="incident.id"
           >
-          <router-link :to="{ name: 'IncidentDetail', params: { id: i.id }}">
-              <IncidentListItem :incident="i" />
+          <router-link :to="{ name: 'IncidentDetail', params: { id: incident.id }}">
+              <IncidentListItem :incident="incident" />
           </router-link>
           </div>
         </div>
+        <div
+        class="card status"
+        v-for="site in sites"
+        :key="site.id"
+        > 
+        <SiteStatus :site="site"></SiteStatus>
+      </div>
       </div>
   </div>
 </template>
 
 <script>
 import IncidentListItem from './IncidentListItem.vue'
+import SiteStatus from './SiteStatus.vue'
 export default {
   name: 'Home',
-  components: {
-      IncidentListItem
-  },
+  components: {IncidentListItem, SiteStatus},
   data () {
       return {
-              openIncidents: [{'id': 1, 'title': 'Connectivity Issues', update_set: [{'description': 'We have noticed some connectivity issues', 'date': '2019-02-02 13:00:12', 'status': 'Investigating'}]}]
+        openIncidents: [{'id': 1, 'title': 'Connectivity Issues', update_set: [{'description': 'We have noticed some connectivity issues', 'date': '2019-02-02 13:00:12', 'status': 'Investigating'}]}],
+        sites: [{'title': 'API', 'status': 'up', 'uptime_set': [{'date': '2018-07-12T13:42:58.085870', 'id': 1, 'response_time': 352, 'status': 'up'}, {'date': '2018-07-12T13:43:58.085870', 'id': 2, 'response_time': 231, 'status': 'up'}, {'date': '2018-07-12T13:44:58.085870', 'id': 3, 'response_time': 123, 'status': 'down'}, {'date': '2018-07-12T13:45:58.085870', 'id': 4, 'response_time': 344, 'status': 'issue'}]}]
       }
   }
 }
@@ -36,5 +43,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.card.status {
+    margin-bottom: 10px;
+}
 </style>
