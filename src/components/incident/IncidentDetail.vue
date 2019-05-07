022bfc641f6b6a4562d12ab0d28112e137b791ac
JC Romualdo <juan.romualdo@zoro.com>
2019-05-07 11:57:18 -0500

    incident details

--- src/components/incident/IncidentDetail.vue
+++ src/components/incident/IncidentDetail.vue
@@ -4,11 +4,11 @@
     <h1>{{incident.title}}</h1>
     <div
       class="incident_item"
-      v-for="i in incident.update_set"
-      :key="i.id"
+      v-for="incident in incident.update_set"
+      :key="incident.id"
      >
-       <p>{{i.description}}</p>
-       <div class="half">{{i.date}}</div><div class="half right">Status: {{i.status}}</div>
+       <p>{{incident.description}}</p>
+       <div class="half">{{incident.date}}</div> <div class="half right">Status: {{incident.status}}</div>
      </div>
   </div>
   <router-link :to="{ name: 'Home'}">
@@ -18,7 +18,10 @@
 </template>
 
 <script>
+import { timeFormatter } from '@/mixins/DateTimeFilters'
 export default {
+    name: 'IncidentDetail',
+    mixins: [timeFormatter],
     data () {
         return {
             incidents: [{id: 1, title: 'Connectivity issues', update_set: [{'title': 'Connectivity issues: slow response time API', 'description': 'We are experiencing connectivity issues on our API. We are investigating this issue and will keep you up to date on this.', 'date': '2018-9-12 12:30:00', 'status': 'identified'}, {'title': 'We are still investigating this issue.', 'description': 'We are still working hard on investigating this issue. We appreciate your support and patience.', 'date': '2018-8-23 12:23:23', 'status': 'investigating'}]}],
