ed155aa63b4e64f80d85907671bd25e050a2757c
JC Romualdo <juan.romualdo@zoro.com>
2019-05-14 15:01:15 -0500

    websocket configurations

--- src/main.js
+++ src/main.js
@@ -7,12 +7,21 @@ import fontawesome from '@fortawesome/fontawesome'
 import regular from '@fortawesome/fontawesome-free-regular'
 import solid from '@fortawesome/fontawesome-free-solid'
 import store from './store'
+import VueNativeSock from 'vue-native-websocket'
 
 fontawesome.library.add(regular)
 fontawesome.library.add(solid)
-
 Vue.config.productionTip = false
 
+if (process.env.NODE_ENV === 'development') {
+  Vue.prototype.webSocketBaseURL = 'ws://localhost:8000/ws/'
+}
+
+Vue.use(VueNativeSock, Vue.prototype.webSocketBaseURL, {
+  store: store,
+  reconnection: true,
+  reconnectionDelay: 3000
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
