d225cf987177d43e38ababbfda7131caea0a170b
JC Romualdo <juan.romualdo@zoro.com>
2019-05-14 15:02:18 -0500

    mutations and websocket state

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,37 @@ const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
   strict: debug,
   state: {
+    socket: {
+      isConnected: true,
+      message: '',
+      reconnectError: false
+    }
   },
   mutations: {
+    SOCKET_ONOPEN (state, event) {
+      console.log('What can I do for you?')
+      state.socket.isConnected = true
+    },
+    SOCKET_ONCLOSE (state, event) {
+      console.log('Oh dang, I can\'t reach you anymore!')
+      state.socket.isConnected = false
+    },
+    SOCKET_ONERROR (state, event) {
+      console.error(state, event)
+    },
+    // default handler called for all methods
+    SOCKET_ONMESSAGE (state, message) {
+      state.socket.message = message
+    },
+    // mutations for reconnect methods
+    SOCKET_RECONNECT (state, count) {
+      console.info(state, count)
+    },
+    SOCKET_RECONNECT_ERROR (state) {
+      console.log('we are back up and sailing guys! Nothing to worry about.')
+      state.socket.reconnectError = true
+    }
   },
   actions: {
-
   }
-
 })
